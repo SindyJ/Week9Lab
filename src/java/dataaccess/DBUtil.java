342d95389efa6b52be8d35da0e2f35899d5fbe3b
726772 <726772@SAIT225751IT.ACDM.DS.SAIT.CA>
2020-11-08 19:12:40 -0700

    Removed ConnectionPool class and updated the DBUtil for JPA

--- src/java/dataaccess/DBUtil.java
+++ src/java/dataaccess/DBUtil.java
@@ -1,34 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dataaccess;
 
-import java.sql.*;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
-/**
- *
- * @author 775653
- */
 public class DBUtil {
-    public static void closePreparedStatement(Statement ps){
-        try {
-            if(ps != null){
-                ps.close();
-            }
-        } catch (SQLException ex) {
-            System.out.println(ex);
-        }
-    }
-    
-    public static void closeResultSet(ResultSet rs){
-        try {
-            if(rs != null){
-                rs.close();
-            }
-        } catch (SQLException ex) {
-            System.out.println(ex);
-        }
+    private static final EntityManagerFactory emf =
+        Persistence.createEntityManagerFactory("Week9LabPU");
+
+    public static EntityManagerFactory getEmFactory() {
+        return emf;
     }
 }
